40cae5bdd771d02974c5a8d6fa0923a804ee8a34
ygubert72 <ygubert72@gmail.com>
2026-06-09 15:29:01 +0500

    Create recipesDB.js

---
+++ modules/recipesDB.js
@@ -0,0 +1,18 @@
+// modules/recipesDB.js
+export const recipesDB = [
+    {
+        id: 1,
+        name: "Яичница с помидорами",
+        cuisine: "european",
+        type: "main",
+        ingredients: ["Яйца", "Помидоры", "Масло сливочное"],
+        steps: [
+            "1. Разогрей сковороду",
+            "2. Обжарь помидоры",
+            "3. Добавь яйца",
+            "4. Жарь 3-5 минут"
+        ],
+        tip: "Накрой крышкой"
+    }
+];
+export default recipesDB;
