b06f91ef45e75d04b0ec8cf9548a9a0a6692f044
ygubert72 <ygubert72@gmail.com>
2026-06-11 11:30:40 +0500

    Update index.js

--- modules/recipes/index.js
+++ modules/recipes/index.js
@@ -1,17 +1,20 @@
 // modules/recipes/index.js
 import { salads } from './salads.js';
 import { snacks } from './snacks.js';
+import { soups } from './soups.js';
 
 // Объединяем все рецепты в один массив
 export const allRecipes = [
     ...salads,    // 50 салатов (id: 101-150)
-    ...snacks     // 50 закусок (id: 201-250)
+    ...snacks,    // 50 закусок (id: 201-250)
+    ...soups      // 50 супов (id: 301-350)
 ];
 
 // Экспортируем отдельно по категориям
 export const recipesByType = {
     salad: salads,
-    snack: snacks
+    snack: snacks,
+    soup: soups
 };
 
 export default allRecipes;
